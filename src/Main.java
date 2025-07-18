1acbe6a1b5743877c1638213c036579615cf11f0
joneidson <joneidsonvictorjc@gmail.com>
2025-07-18 16:25:53 -0300

    Remoção de comentarios desnecessarios

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@
 public class Main {
     public static void main(String[] args) {
         String nome = "victor";
-        int idade = 28;//TIP Press <shortcut actionId="ShowInt
-        String cidade = "Salvador-Ba";// entionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        int idade = 28;
+        String cidade = "Salvador-Ba";//
+
         System.out.printf("Olá, seja bem-vindo!!\n");
         System.out.println("Inicio de uma nova jornada, aprendendo Java!!");
         System.out.printf("Eu sou %s e tenho %d anos de idade sou de %s", nome, idade, cidade);
