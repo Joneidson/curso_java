2c1586f34d2212cd8f804247525f980560fff3a4
joneidson <joneidsonvictorjc@gmail.com>
2025-07-18 16:23:11 -0300

    Atuli da pagina inicial

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         int idade = 28;//TIP Press <shortcut actionId="ShowInt
         String cidade = "Salvador-Ba";// entionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!\n");
+        System.out.printf("Olá, seja bem-vindo!!\n");
         System.out.println("Inicio de uma nova jornada, aprendendo Java!!");
         System.out.printf("Eu sou %s e tenho %d anos de idade sou de %s", nome, idade, cidade);
     }
