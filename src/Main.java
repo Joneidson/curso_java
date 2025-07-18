bd5a93846daf4072facaa99dbe3a3dd28bb0a346
joneidson <joneidsonvictorjc@gmail.com>
2025-07-18 16:12:08 -0300

    Criação de apresentação inicial

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        String nome = "victor";
+        int idade = 28;//TIP Press <shortcut actionId="ShowInt
+        String cidade = "Salvador-Ba";// entionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
+        System.out.println("Inicio de uma nova jornada, aprendendo Java!!");
+        System.out.printf("Eu sou %s e tenho %d anos de idade sou de %s", nome, idade, cidade);
     }
 }
