8bb551b0491e9f3c508418fc6bdd52899bee8022
joneidson <joneidsonvictorjc@gmail.com>
2025-07-22 21:21:18 -0300

    Feature: Mudanças de titulos

--- src/ExecicioCondicionais.java
+++ src/ExecicioCondicionais.java
@@ -1,10 +1,15 @@
 import java.util.Locale;
 import java.util.Scanner;
-
+/*
+Uma operadora de telefonia cobra R$ 50.00 por um plano básico que dá direito a 100 minutos de telefone.
+Cada minuto que exceder a franquia de 100 minutos custa R$ 2.00.
+Fazer um programa para ler a quantidade de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago.
+*/
 public class ExecicioCondicionais {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
+        Locale.setDefault(Locale.US); // alter de , para .
         Scanner sc = new Scanner(System.in);
+        // Título
         System.out.println("=".repeat(20));
         System.out.println("Cálculo de Fatura");
         System.out.println("=".repeat(20));
