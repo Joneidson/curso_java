f0ca51c4044fca5d99ab2316950f7b8799d1c8e8
joneidson <joneidsonvictorjc@gmail.com>
2025-07-27 20:48:41 -0300

    Feature: alteração na sitex

--- src/WhileExercicioFixacao.java
+++ src/WhileExercicioFixacao.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ExercicioFixacaoWhile {
+public class WhileExercicioFixacao {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = 0;
