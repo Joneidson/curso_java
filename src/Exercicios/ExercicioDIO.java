fc3414496ef5b3cb7ebad3f3a233da191ca8971a
joneidson <joneidsonvictorjc@gmail.com>
2026-04-13 21:03:18 -0300

    Incremento da atividade do curso da DIO

---
+++ src/Exercicios/ExercicioDIO.java
@@ -0,0 +1,29 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+
+public class ExercicioDIO{
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       if (sc.hasNext()){
+        String linha = sc.nextLine( );
+        String[] partes = linha.split(" ");
+        if (partes.length == 2) {
+            String name = partes[0];
+            int age = Integer.parseInt(partes[1]);
+
+            if (!name.isEmpty() && age >= 18) {
+                System.out.println("Cadastro Aprovado");
+            }else {
+                System.out.println("Cadastro reprovado");
+            }
+        }else {
+            System.out.println("Cadastro reprovado");
+        }
+
+       }
+        sc.close( );
+    }
+}
